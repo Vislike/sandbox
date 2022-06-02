c872ba43c396a9a2227020e97f26645373ea18bb
Vislike <rickard000@hotmail.com>
2022-06-02 15:32:37 +0200

    Initial commit

---
+++ src/main/java/sandbox/main/App.java
@@ -0,0 +1,10 @@
+package sandbox.main;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		System.out.println("This is a sandbox, check out different packages.");
+	}
+
+}
